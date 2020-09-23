dd92fad2d0ae8526e24ec15a909728d5ac2fd619
Daniil <daniel217032001@gmail.com>
2020-09-23 23:54:17 +0300

    update examples

--- main.cpp
+++ main.cpp
@@ -8,20 +8,21 @@ using std::string;
 int main() {
     auto *d = new Dictionary();
 
-    d->add("Dog", "Собака");
-    d->add("Cat", "Кошка");
-    d->add("Bed", "Кровать");
+    d->put("Dog", "Собака");
+    d->put("Cat", "Кошка");
+    d->put("Bed", "Кровать");
     d->print();
+    cout << "\n";
 
-    d->add("dog", "Собакен");
-    d->add("DOG", "Собачка");
+    d->put("dog", "Собакен");
+    d->put("DOG", "Собачка");
+    d->update("cat", "Кот");
     d->print();
+    cout << "\n";
 
     cout << "Dog - " << d->get("Dog") << "\n";
     cout << "Cat - " << d->get("Cat") << "\n";
-
-    d->update("cat", "Кот");
-    d->print();
+    cout << "\n";
 
     unsigned int length;
     string *values = d->keys(length);
@@ -29,6 +30,7 @@ int main() {
         cout << values[i] << " ";
     }
     cout << "\n";
+    delete[] values;
 
     values = d->values(length);
     for (unsigned int i = 0; i < length; ++i) {
@@ -36,17 +38,15 @@ int main() {
     }
     cout << "\n\n";
 
-    cout << "Количество: " << d->count() << "\n";
+    cout << "Количество: " << d->count() << "\n\n";
     d->remove("cat");
     d->print();
-    cout << "Количество: " << d->count() << "\n";
+    cout << "Количество: " << d->count() << "\n\n";
 
     d->remove("bed");
-    cout << "Пустой: " << (d->isEmpty() ? "Да" : "Нет") << "\n";
+    cout << "Пустой: " << (d->is_empty() ? "Да" : "Нет") << "\n";
     d->remove("dog");
-    cout << "Пустой: " << (d->isEmpty() ? "Да" : "Нет") << "\n";
-
-    d->add("dependency", "Зависимость");
+    cout << "Пустой: " << (d->is_empty() ? "Да" : "Нет") << "\n\n";
 
     delete d;
     return 0;
